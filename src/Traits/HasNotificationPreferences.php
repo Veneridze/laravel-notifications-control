bdf45f218bffeb481e23bac38cd15b1b3d52fc9c
Veneridze <viacheslav.kopaev@yandex.ru>
2024-10-12 13:11:18 +0300

    rename trait HasNotifications

--- src/Traits/HasNotificationPreferences.php
+++ src/Traits/HasNotificationPreferences.php
@@ -1,7 +1,10 @@
 <?php
 namespace Veneridze\NotificationsControl\Traits;
 use Illuminate\Notifications\Notification;
-class HasNotifications {
+use Illuminate\Support\Facades\App;
+use Veneridze\ModelTypes\Exceptions\WrongWay;
+use Veneridze\NotificationsControl\NotificationsControl;
+class HasNotificationPreferences {
     /**
      * Summary of canNotify
      * @param Notification $notification
@@ -18,6 +21,9 @@ class HasNotifications {
     }
 
     public function canNotify(Notification $notification, string $way = null): bool {
+        if(!App::make(NotificationsControl::class)->wayExist($way)) {
+            throw new WrongWay("Канал уведомлений {$way} не существует");
+        }
         $notifications = $this->getUserNotifications();
         return array_key_exists($notification::class, $notifications) 
         && ($way ? in_array($way, $notifications[$notification::class]) : count($notifications[$notification::class]) > 0);
@@ -29,6 +35,12 @@ class HasNotifications {
      * @return void
      */
     public function setNotifyPreferences(Notification $notification, array $ways): void {
+        foreach ($ways as $way) {
+            if(!App::make(NotificationsControl::class)->wayExist($way)) {
+                throw new WrongWay("Канал уведомлений {$way} не существует");
+            }
+        }
+
         $this->update([
             'notificationPreferences' => [
                 ...$this->getUserNotifications(),
